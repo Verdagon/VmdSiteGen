fe7aa1395dcc1feb90dcccf535b66b0e9d2d825b
Evan Ovadia <>
2023-05-07 10:42:45 -0500

    Works with fixed .26

--- build.sh
+++ build.sh
@@ -1,9 +1,25 @@
 VALEC_DIR="$1"
 if [ "$VALEC_DIR" == "" ]; then
   echo "Please supply the valec directory."
-  echo "Example: ~/ValeCompiler-0.1.3.3"
+  echo "Example: ./build.sh ~/ValeCompiler-0.1.3.3 ~/VmdParse ~/ParseIter"
   exit
 fi
 shift
 
-$VALEC_DIR/valec build vmdsitegen=src vmdparse=~/VmdParse/src parseiter=~/ParseIter/src --output_dir build --region_override resilient-v3 -o vmdsitegen $@
+VMD_PARSE_DIR="$1"
+if [ "$VMD_PARSE_DIR" == "" ]; then
+  echo "Please supply the VmdParse directory."
+  echo "Example: ./build.sh ~/ValeCompiler-0.1.3.3 ~/VmdParse ~/ParseIter"
+  exit
+fi
+shift
+
+PARSE_ITER_DIR="$1"
+if [ "$PARSE_ITER_DIR" == "" ]; then
+  echo "Please supply the ParseIter directory."
+  echo "Example: ./build.sh ~/ValeCompiler-0.1.3.3 ~/VmdParse ~/ParseIter"
+  exit
+fi
+shift
+
+$VALEC_DIR/valec build vmdsitegen=src vmdparse=$VMD_PARSE_DIR/src parseiter=$PARSE_ITER_DIR/src --output_dir build --region_override resilient-v3 -o vmdsitegen $@
